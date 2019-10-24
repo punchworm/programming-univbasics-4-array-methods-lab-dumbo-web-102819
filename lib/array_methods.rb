48414c4ceeec16239d5215d2f78f38e9167f9774
Nia Laureano <nialaureano@icloud.com>
2019-10-24 18:10:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,7 @@
 def using_include(array, element)
-def using_include(array, element)
-  array.include?(element)
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    element1 = "wow"
+    element2 = "Tardis"
 end
 end
 
